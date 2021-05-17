8a5fbd8691972e70672225ef86e124c92a719f49
Siu Kei Tam <tam.s.k.92@gmail.com>
2021-05-17 13:51:15 +0100

    Updated the styling to not use react-bootstrap card components

--- src/components/CourseCards.js
+++ src/components/CourseCards.js
@@ -1,33 +1,50 @@
 import React from 'react'
-import '../styles/MainActivities.scss'
-import { Card, CardDeck } from 'react-bootstrap'
+import '../styles/Main.scss'
+// import { Card, CardDeck } from 'react-bootstrap'
 import Bruschetta from '../images/Bruschetta.jpg'
 import Cheesecake from '../images/Cheesecake.jpg'
 
+const cardBackgroundStyle = {
+  backgroundImage: `url(${Bruschetta})`
+}
+
 export const CourseCards = () => {
   return (
-    <div className="MainActivities-Weekly-Wrapper">
-      <h2>This Week's Events</h2>
-      <div className="MainActivities-Weekly-Container">
-        <CardDeck>
-          <Card bg="card-theme">
-            <Card.Img variant="top" src={Bruschetta} />
-            <Card.Body>
-              <Card.Title>Bruschetta</Card.Title>
-              <Card.Text className="date">Jan 7, 2020</Card.Text>
-              <Card.Text className="price">$15.00/person</Card.Text>
-            </Card.Body>
-          </Card>
-          <Card bg="card-theme">
-            <Card.Img variant="top" src={Cheesecake} />
-            <Card.Body>
-              <Card.Title>Scrumptious Salads</Card.Title>
-              <Card.Text className="date">Jan 9, 2020</Card.Text>
-              <Card.Text className="price">$20.00/person</Card.Text>
-            </Card.Body>
-          </Card>
-        </CardDeck>
+    <div className="card-container">
+      <div className="card" style={cardBackgroundStyle}>
+        <h2>Bruschetta</h2>
+        <h3>£3.50</h3>
+      </div>
+      <div className="card" style={cardBackgroundStyle}>
+        <h2>Bruschetta</h2>
+        <h3>£3.50</h3>
+      </div>
+      <div className="card" style={cardBackgroundStyle}>
+        <h2>Bruschetta</h2>
+        <h3>£3.50</h3>
       </div>
     </div>
+    // <div className="MainActivities-Weekly-Wrapper">
+    //   <div className="MainActivities-Weekly-Container">
+    //     <CardDeck>
+    //       <Card bg="card-theme" className="dish">
+    //         <Card.Img variant="top" src={Bruschetta} />
+    //         <Card.Body>
+    //           <Card.Title>Bruschetta</Card.Title>
+    //           <Card.Text className="date">Jan 7, 2020</Card.Text>
+    //           <Card.Text className="price">$15.00/person</Card.Text>
+    //         </Card.Body>
+    //       </Card>
+    //       <Card bg="card-theme" className="dish">
+    //         <Card.Img variant="top" src={Cheesecake} />
+    //         <Card.Body>
+    //           <Card.Title>Scrumptious Salads</Card.Title>
+    //           <Card.Text className="date">Jan 9, 2020</Card.Text>
+    //           <Card.Text className="price">$20.00/person</Card.Text>
+    //         </Card.Body>
+    //       </Card>
+    //     </CardDeck>
+    //   </div>
+    // </div>
   )
 }
