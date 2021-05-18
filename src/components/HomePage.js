92d4c3340bcf25fbf5262920912abe46094a465c
Siu Kei Tam <tam.s.k.92@gmail.com>
2021-05-18 23:02:51 +0100

    Added logic to add/remove mains and desserts, update total to state

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -18,12 +18,9 @@ export const HomePage = () => {
   //Update state by adding/remove selected menu item
   //Calculate the new total value and update state
   const handleClick = (e) => {
-    // console.log(e.target.id)
-    // console.log(e.target.classList.contains('starters'))
     let selectedItem = []
     let itemToRemoveIndex = 0
     if (e.target.classList.contains('starters')){
-
 
       //Access array of menu items depending on course selected
       const courseItems = menu['starters']
@@ -34,11 +31,9 @@ export const HomePage = () => {
         }
       })
 
-
       // Add 'active' class to menu items if not selected otherwise remove 'active' class
       if (!e.target.classList.contains('active')) {
         e.target.classList.add('active')
-
 
         //Check which diner ordered
         if (e.target.classList.contains('1')) {
@@ -55,49 +50,101 @@ export const HomePage = () => {
       } else {
         e.target.classList.remove('active')
 
+        //Check which diner ordered
         if (e.target.classList.contains('1')) {
+          //Find index of selected item in state array
           itemToRemoveIndex = diner1starter.findIndex((item) => {
             return item.name === e.target.id
           })
+
+          //Remove selected item from state array and update state and total
           diner1starter.splice(itemToRemoveIndex, 1)
           setdiner1starter(diner1starter)
           settotal(total - selectedItem[0].price)
         } else if (e.target.classList.contains('2')) {
-
+          itemToRemoveIndex = diner1starter.findIndex((item) => {
+            return item.name === e.target.id
+          })
+          diner2starter.splice(itemToRemoveIndex, 1)
+          setdiner2starter(diner2starter)
+          settotal(total - selectedItem[0].price)
         }
       }
 
-
     } else if (e.target.classList.contains('mains')) {
-      if (!e.target.classList.contains('active')) {
-        e.target.classList.add('active')
-      } else {
-        e.target.classList.remove('active')
-      }
-
       const courseItems = menu['mains']
       selectedItem = courseItems.filter((item) => {
         if (e.target.id === item.name) {
           return item
         }
       })
-    } else if (e.target.classList.contains('desserts')) {
       if (!e.target.classList.contains('active')) {
         e.target.classList.add('active')
+        if (e.target.classList.contains('1')) {
+          diner1main.push(selectedItem[0])
+          setdiner1main(diner1main)
+          settotal(total + selectedItem[0].price)
+        } else if (e.target.classList.contains('2')) {
+          diner2main.push(selectedItem[0])
+          setdiner2main(diner2main)
+          settotal(total + selectedItem[0].price)
+        }
       } else {
         e.target.classList.remove('active')
+        if (e.target.classList.contains('1')) {
+          itemToRemoveIndex = diner1main.findIndex((item) => {
+            return item.name === e.target.id
+          })
+          diner1main.splice(itemToRemoveIndex, 1)
+          setdiner1main(diner1main)
+          settotal(total - selectedItem[0].price)
+        } else if (e.target.classList.contains('2')) {
+          itemToRemoveIndex = diner1main.findIndex((item) => {
+            return item.name === e.target.id
+          })
+          diner2main.splice(itemToRemoveIndex, 1)
+          setdiner2main(diner2main)
+          settotal(total - selectedItem[0].price)
+        }
       }
-
+    } else if (e.target.classList.contains('desserts')) {
       const courseItems = menu['desserts']
       selectedItem = courseItems.filter((item) => {
         if (e.target.id === item.name) {
           return item
         }
       })
+      if (!e.target.classList.contains('active')) {
+        e.target.classList.add('active')
+        if (e.target.classList.contains('1')) {
+          diner1dessert.push(selectedItem[0])
+          setdiner1dessert(diner1dessert)
+          settotal(total + selectedItem[0].price)
+        } else if (e.target.classList.contains('2')) {
+          diner2dessert.push(selectedItem[0])
+          setdiner2dessert(diner2dessert)
+          settotal(total + selectedItem[0].price)
+        }
+      } else {
+        e.target.classList.remove('active')
+        if (e.target.classList.contains('1')) {
+          itemToRemoveIndex = diner1dessert.findIndex((item) => {
+            return item.name === e.target.id
+          })
+          diner1dessert.splice(itemToRemoveIndex, 1)
+          setdiner1dessert(diner1dessert)
+          settotal(total - selectedItem[0].price)
+        } else if (e.target.classList.contains('2')) {
+          itemToRemoveIndex = diner1dessert.findIndex((item) => {
+            return item.name === e.target.id
+          })
+          diner2dessert.splice(itemToRemoveIndex, 1)
+          setdiner2dessert(diner2dessert)
+          settotal(total - selectedItem[0].price)
+        }
+      }
     }
 
-    //Set selected item to state
-    console.log('Final Item array is:', selectedItem)
   }
 
   //Check whether selection is invalid based on rules
@@ -152,6 +199,12 @@ export const HomePage = () => {
 
   console.log('Total value state is:',total)
   console.log('Diner 1 Starter State is:',diner1starter)
+  console.log('Diner 1 Main State is:',diner1main)
+  console.log('Diner 1 Dessert State is:',diner1dessert)
+  console.log('Diner 2 Starter State is:',diner2starter)
+  console.log('Diner 2 Main State is:',diner2main)
+  console.log('Diner 2 Dessert State is:',diner2dessert)
+
   return (
     <div className="homepage-container">
       <TotalValue total={total}/>
